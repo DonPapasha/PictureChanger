5b9233624a77d094a6d56959ec355ff9588bfce5
DonPapasha <maltzev.pav@yandex.ru>
2023-10-24 22:47:29 +0300

    Moments output

--- PictureChanger.cpp
+++ PictureChanger.cpp
@@ -131,17 +131,24 @@ void Difference()
     Moments momUserImg = moments(threshUserImg);
     Moments momNewImg = moments(threshNewImg);
 
+    printf("- Моменты изначального изображения |m00 = %f| |m10 = %f| |m01 = %f| |m20 = %f| |m11 = %f| \n |m02 = %f| |m30 = %f| |m21 = %f| |m12 = %f| |m03 = %f| \n", momUserImg.m00, momUserImg.m10, momUserImg.m01, momUserImg.m20, momUserImg.m11, momUserImg.m02, momUserImg.m30, momUserImg.m21, momUserImg.m12, momUserImg.m03);
+    printf("Центральные  |mu20 = % f| |mu11 = % f| |mu02 = % f| |mu30 = % f| |mu21 = % f | \n |mu12 = % f| |mu03 = % f| \n", momUserImg.mu20, momUserImg.mu11, momUserImg.mu02, momUserImg.mu02, momUserImg.mu30, momUserImg.mu21, momUserImg.mu12, momUserImg.mu03);
+    printf("Нормированные |nu20 = %f| |nu11 = %f| |nu02 = %f| |nu30 = %f| |nu21 = %f| |nu12 = %f| |nu03 = %f|\n", momUserImg.nu20, momUserImg.nu11, momUserImg.nu02, momUserImg.nu30, momUserImg.nu21, momUserImg.nu12, momUserImg.nu03);
+    printf("- Моменты изменённого  изображения |m00 = %f| |m10 = %f| |m01 = %f| |m20 = %f| |m11 = %f| \n |m02 = %f| |m30 = %f| |m021 = %f| |m21 = %f| |m03 = %f|\n", momNewImg.m00, momNewImg.m10, momNewImg.m01, momNewImg.m20, momNewImg.m11, momNewImg.m02, momNewImg.m30, momNewImg.m21, momNewImg.m12, momNewImg.m03);
+    printf("Центральные  |mu20 = % f| |mu11 = % f| |mu02 = % f| |mu30 = % f| |mu21 = % f | \n |mu12 = % f| |mu03 = % f| \n", momNewImg.mu20, momNewImg.mu11, momNewImg.mu02, momNewImg.mu02, momNewImg.mu30, momNewImg.mu21, momNewImg.mu12, momNewImg.mu03);
+    printf("Нормированные |nu20 = %f| |nu11 = %f| |nu02 = %f| |nu30 = %f| |nu21 = %f| |nu12 = %f| |nu03 = %f|\n", momNewImg.nu20, momNewImg.nu11, momNewImg.nu02, momNewImg.nu30, momNewImg.nu21, momNewImg.nu12, momNewImg.nu03);
+
     double huMomUserImg[7];
     double huMomNewImg[7];
     HuMoments(momUserImg, huMomUserImg);
     HuMoments(momNewImg, huMomNewImg);
 
     for (int i = 0; i < 7; i++) {
-        cout << "Моменты изначального изображения H[" << i <<"] : " << huMomUserImg[i] << endl;
+        cout << "Xy Моменты изначального изображения H[" << i <<"] : " << huMomUserImg[i] << endl;
     }
     cout << "" << endl;
     for (int i = 0; i < 7; i++) {
-        cout << "Моменты изменённого изображения H[" << i << "] : " << huMomNewImg[i] << endl;
+        cout << "Xy Моменты изменённого изображения H[" << i << "] : " << huMomNewImg[i] << endl;
     }
     
     double _difference1 = matchShapes(threshUserImg, threshNewImg, CONTOURS_MATCH_I1, 0);
